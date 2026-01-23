429d45c178d65c8816dfee519a05f8ff960515f4
Muhammad Irfan Mubasyir <muhammadirfanmubasyir@gmail.com>
2026-01-23 16:05:44 +0700

    Initial commit

---
+++ src/main/java/com/irfan/microservices/product/service/ProductService.java
@@ -0,0 +1,41 @@
+package com.irfan.microservices.product.service;
+
+import com.irfan.microservices.product.dto.ProductRequest;
+import com.irfan.microservices.product.dto.ProductResponse;
+import com.irfan.microservices.product.model.Product;
+import com.irfan.microservices.product.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class ProductService {
+    private final ProductRepository productRepository;
+
+    public ProductResponse createProduct(ProductRequest productRequest) {
+        Product product = Product.builder()
+                .name(productRequest.name())
+                .description(productRequest.description())
+                .price(productRequest.price())
+                .build();
+
+        Product newProduct = productRepository.save(product);
+        log.info("PRODUCT IS CREATED SUCCESSFULLY !");
+
+        return new ProductResponse(newProduct.getId(), newProduct.getName(), newProduct.getDescription(), newProduct.getPrice());
+
+    }
+
+    public List<ProductResponse> getAllProducts() {
+        return productRepository.findAll()
+                .stream()
+                .map(product -> new ProductResponse(product.getId(), product.getName(), product.getDescription(), product.getPrice()))
+                .toList();
+
+    }
+}
